30cff42919f430f1f9a29f1254d96e7278cee071
IronBlood <aplshenyang@gmail.com>
2020-08-20 21:53:00 +0800

    to localize by detecting browser language setting, providing en/ru/zh i18n files

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+var languageSet = new Set();
 /*!
  * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
  * Copyright 2013-2020 Start Bootstrap
@@ -45,4 +46,31 @@
 		document.querySelector("#cert-detail").src = this.src;
 		$("#cert-modal").modal();
 	});
+
+	let lang = navigator.language.substring(0, 2);
+	if (lang == "ru" || lang == "uk" || lang == "be") lang = "ru";
+	else if (lang == "zh") { }
+	else lang = "en"
+	update_text(lang);
+
+	$("#btn-e").click(function() { update_text("en"); });
+	$("#btn-r").click(function() { update_text("ru"); });
+	$("#btn-c").click(function() { update_text("zh"); });
 })(jQuery); // End of use strict
+
+function update_text(locale) {
+	if (locale == $.i18n().locale) {
+		return;
+	}
+	if (languageSet.has(locale)) {
+		$.i18n().locale = locale;
+		$("body").i18n(locale);
+	}
+	else {
+		$.i18n().load("i18n/" + locale + ".json", locale).then(function() {
+			languageSet.add(locale);
+			$.i18n().locale = locale;
+			$("body").i18n(locale);
+		});
+	}
+}
